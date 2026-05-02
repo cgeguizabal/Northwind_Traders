23d367d986b27de40097c97ba456aaa8c03925f7
Guillermo Eguizabal <cgeguizabal@gmail.com>
2026-05-01 19:36:51 -0600

    Package

--- Northwind_Traders_BackEnd/src/NorthwindTraders.Infrastructure/obj/Debug/net10.0/NorthwindTraders.Infrastructure.AssemblyInfo.cs
+++ Northwind_Traders_BackEnd/src/NorthwindTraders.Infrastructure/obj/Debug/net10.0/NorthwindTraders.Infrastructure.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NorthwindTraders.Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a6bbce7162327c39dbd1314ac3dab57c4ff72f21")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+68ce4baa4ba612fe19645c549c6eba68a9a2a6ab")]
 [assembly: System.Reflection.AssemblyProductAttribute("NorthwindTraders.Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NorthwindTraders.Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
