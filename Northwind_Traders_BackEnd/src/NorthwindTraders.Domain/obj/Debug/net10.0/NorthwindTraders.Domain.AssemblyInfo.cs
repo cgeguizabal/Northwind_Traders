1db1e2fb1636dbb69aa4614354591245e4f5975f
Guillermo Eguizabal <cgeguizabal@gmail.com>
2026-05-03 18:10:22 -0600

    built

--- Northwind_Traders_BackEnd/src/NorthwindTraders.Domain/obj/Debug/net10.0/NorthwindTraders.Domain.AssemblyInfo.cs
+++ Northwind_Traders_BackEnd/src/NorthwindTraders.Domain/obj/Debug/net10.0/NorthwindTraders.Domain.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NorthwindTraders.Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bcbde441292c1a151d178a771f714a6c28999386")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8be1b2672c3000c8981c6dc67cbbd3ff546f2a00")]
 [assembly: System.Reflection.AssemblyProductAttribute("NorthwindTraders.Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NorthwindTraders.Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
