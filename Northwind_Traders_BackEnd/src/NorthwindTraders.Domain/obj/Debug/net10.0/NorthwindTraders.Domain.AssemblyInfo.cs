23a1369a0856071ebb0547c6ef6d6659cfd371a9
Guillermo Eguizabal <cgeguizabal@gmail.com>
2026-05-04 23:16:27 -0600

    built

--- Northwind_Traders_BackEnd/src/NorthwindTraders.Domain/obj/Debug/net10.0/NorthwindTraders.Domain.AssemblyInfo.cs
+++ Northwind_Traders_BackEnd/src/NorthwindTraders.Domain/obj/Debug/net10.0/NorthwindTraders.Domain.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NorthwindTraders.Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e445acb42f93e94ab8faf51bf57bf904b2c99221")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+938e121cd16e32c3591d1a0261483d57bc083644")]
 [assembly: System.Reflection.AssemblyProductAttribute("NorthwindTraders.Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NorthwindTraders.Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ Northwind_Traders_BackEnd/src/NorthwindTraders.Infrastructure/Migrations/20260505050834_SetMustChangePasswordForExisting.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace NorthwindTraders.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class SetMustChangePasswordForExisting : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            // All existing employees were seeded with a default password.
+            // Force them to change it on their next login.
+            migrationBuilder.Sql("UPDATE [Employees] SET [MustChangePassword] = 1 WHERE [PasswordHash] IS NOT NULL");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("UPDATE [Employees] SET [MustChangePassword] = 0");
+        }
+    }
+}
